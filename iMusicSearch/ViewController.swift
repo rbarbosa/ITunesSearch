05c3c6903548c67de7c9170a7d4123b51570fd2c
Rui Barbosa <rpbarbosa@gmail.com>
2016-11-05 16:06:06 +0000

    Initial Commit

---
+++ iMusicSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMusicSearch
+//
+//  Created by Rui Pedro Barbosa on 05/11/2016.
+//  Copyright © 2016 Rui Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
